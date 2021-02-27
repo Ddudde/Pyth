a2a803003eb9ee440530326db5764605e4edd413
Ddudde <BAX.152MM@yandex.ru>
2021-02-27 12:36:41 +0300

    pract2#final

--- ru/mirea/pract2.py
+++ ru/mirea/pract2.py
@@ -35,8 +35,37 @@ def perv(matr):
         matr[0][i] = matr[0][i].replace('@', '[at]')
 
 
-def f21(x):
-    return 0
+def f21(f):
+    rez = -1
+    if f[2] == 'dylan':
+        rez = 9
+    if f[2] == 'tcsh':
+        rez = 10
+    if (f[3] == 'arc') & (rez == -1):
+        rez = 11
+    if rez == -1:
+        if f[0] == 2001:
+            if f[4] == 2000:
+                rez = 0
+            if f[4] == 1997:
+                rez = 1
+            if f[4] == 1965:
+                rez = 2
+        if f[0] == 1973:
+            if f[4] == 2000:
+                rez = 3
+            if f[4] == 1997:
+                rez = 4
+            if f[4] == 1965:
+                rez = 5
+        if f[0] == 1995:
+            if f[4] == 2000:
+                rez = 6
+            if f[4] == 1997:
+                rez = 7
+            if f[4] == 1965:
+                rez = 8
+    return rez
 
 
 def f22(x):
